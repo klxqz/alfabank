28f259617b900f88936826cccb029a9e1e11dac8
klxqz <klxqz@mail.ru>
2013-11-04 17:11:38 +0600

    создание репозитория

---
+++ lib/config/plugin.php
@@ -0,0 +1,10 @@
+<?php
+return array(
+    'name'        => 'Альфа банк',
+    'description' => 'Оплата Банковскими картами через альфа банк',
+    'icon'        => 'img/alfabank16.png',
+    'logo'        => 'img/alfabank.png',
+    'vendor' => 903438,
+    'version' => '1.0.0',
+    'type' => waPayment::TYPE_ONLINE,
+);
