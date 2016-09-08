29614c1befa17c7babca386b761c38e83ab4f34c
korob <korob@192.168.1.180>
2016-09-08 14:26:51 +0500

    Добавлена возможность выбрать версию ssl протокола

--- lib/config/plugin.php
+++ lib/config/plugin.php
@@ -6,6 +6,6 @@ return array(
     'icon' => 'img/alfabank16.png',
     'logo' => 'img/alfabank.png',
     'vendor' => '985310',
-    'version' => '1.0.3',
+    'version' => '1.0.4',
     'type' => waPayment::TYPE_ONLINE,
 );
--- lib/config/settings.php
+++ lib/config/settings.php
@@ -1,23 +1,37 @@
 <?php
-return array(
 
-    'userName'    => array(
-        'value'        => '',
-        'title'        => 'Логин магазина',
-        'description'  => 'Логин магазина, полученный при подключении',
+return array(
+    'userName' => array(
+        'value' => '',
+        'title' => 'Логин магазина',
+        'description' => 'Логин магазина, полученный при подключении',
         'control_type' => 'input',
     ),
-    'password'    => array(
-        'value'        => '',
-        'title'        => 'Пароль магазина',
-        'description'  => 'Пароль магазина, полученный при подключении',
+    'password' => array(
+        'value' => '',
+        'title' => 'Пароль магазина',
+        'description' => 'Пароль магазина, полученный при подключении',
         'control_type' => 'input',
     ),
-    'sandbox'  => array(
-        'value'        => '1',
-        'title'        => 'Тестовый режим',
-        'description'  => '',
+    'sandbox' => array(
+        'value' => '1',
+        'title' => 'Тестовый режим',
+        'description' => '',
         'control_type' => 'checkbox',
     ),
-
+    'ssl_version' => array(
+        'value' => 0,
+        'title' => 'Версия SSL',
+        'description' => '',
+        'control_type' => 'select',
+        'options' => array(
+            0 => 'Определяется автоматически',
+            1 => 'CURL_SSLVERSION_TLSv1',
+            2 => 'CURL_SSLVERSION_SSLv2',
+            3 => 'CURL_SSLVERSION_SSLv3',
+            4 => 'CURL_SSLVERSION_TLSv1_0',
+            5 => 'CURL_SSLVERSION_TLSv1_1',
+            6 => 'CURL_SSLVERSION_TLSv1_2',
+        )
+    ),
 );
